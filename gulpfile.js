b89afd0b26b641c294e9d3bce8e0349c59090562
Raquel Biondi <raquelsylos@gmail.com>
2024-01-23 10:44:54 -0300

    criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() { 
     return gulp.src('./src/styles/*scss')//com esta função nós apenas recuperamos os arquivos, ainda não estamos compilando o sass. Pra encadear essa compilação do sass aqui, temos que usar o Pipe
@@ -7,7 +8,14 @@ function styles() {
     .pipe(gulp.dest('./dist/css')); //dist --> pasta de destino
 }
 
-exports.default = styles;
+function images() { 
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin()) 
+    .pipe(gulp.dest('./dist/images')); 
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*scss', gulp.parallel(styles)) //passamos os arquivos q serão observados e passamos como parâmetro as funções que serão executadas
 }
